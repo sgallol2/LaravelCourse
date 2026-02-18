abd050a28b62fbb2fe3ed66276524df8ca5392b6
sgallo <sofiagdelarosa@gmail.com>
2026-02-17 19:07:50 -0500

    Tutorial sin actividades adicionales

---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', 'App\Http\Controllers\HomeController@index')->name("home.index");
+Route::get('/about', function () {
+    $data1 = "About us - Online Store";
+    $data2 = "About us";
+    $description = "This is an about page ...";
+    $author = "Developed by: Your Name";
+    return view('home.about')->with("title", $data1)
+      ->with("subtitle", $data2)
+      ->with("description", $description)
+      ->with("author", $author);
+})->name("home.about");
+
+Route::get('/products', 'App\Http\Controllers\ProductController@index')->name("product.index");
+Route::get('/products/{id}', 'App\Http\Controllers\ProductController@show')->name("product.show");
