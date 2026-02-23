cdcad2c3464622d40c34d8ac2fb6e640eb9ba078
sgallo <sofiagdelarosa@gmail.com>
2026-02-22 19:51:06 -0500

    Añadir rutas para la vista de creación

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ Route::get('/about', function () {
       ->with("author", $author);
 })->name("home.about");
 
+
+// Orden importante: rutas específicas antes de las dinámicas
 Route::get('/products', 'App\Http\Controllers\ProductController@index')->name("product.index");
+Route::get('/products/create', 'App\Http\Controllers\ProductController@create')->name('product.create');
 Route::get('/products/{id}', 'App\Http\Controllers\ProductController@show')->name("product.show");
+Route::post('/products', 'App\Http\Controllers\ProductController@save')->name('product.save');
