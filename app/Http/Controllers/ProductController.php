881494749092965fbdc62b005a23d3b2931ded0c
sgallo <sofiagdelarosa@gmail.com>
2026-02-22 19:52:43 -0500

    Añadir metodo create y validate para product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,32 +4,48 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\View\View;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
-    public static $products = [
-        ["id"=>"1", "name"=>"TV", "description"=>"Best TV"],
-        ["id"=>"2", "name"=>"iPhone", "description"=>"Best iPhone"],
-        ["id"=>"3", "name"=>"Chromecast", "description"=>"Best Chromecast"],
-        ["id"=>"4", "name"=>"Glasses", "description"=>"Best Glasses"]
-    ];
 
     public function index(): View
     {
         $viewData = [];
         $viewData["title"] = "Products - Online Store";
         $viewData["subtitle"] =  "List of products";
-        $viewData["products"] = ProductController::$products;
+        $viewData["products"] = Product::all();
         return view('product.index')->with("viewData", $viewData);
     }
 
     public function show(string $id) : View
     {
         $viewData = [];
-        $product = ProductController::$products[$id-1];
+        $product = Product::findOrFail($id);
         $viewData["title"] = $product["name"]." - Online Store";
         $viewData["subtitle"] =  $product["name"]." - Product information";
         $viewData["product"] = $product;
         return view('product.show')->with("viewData", $viewData);
+    }
+
+    public function save(Request $request): \Illuminate\Http\RedirectResponse
+    {
+        $request->validate([
+            "name" => "required",
+            "price" => "required"
+        ]);
+
+        Product::create($request->only(["name","price"]));
+
+        return back();
+
+    }
+
+    public function create(): View
+    {
+        $viewData = [];
+        $viewData["title"] = "Create Product - Online Store";
+        $viewData["subtitle"] = "Create a product";
+        return view('product.create')->with("viewData", $viewData);
     }
 }
