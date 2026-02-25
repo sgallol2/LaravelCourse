51d80e439da651a9e9f0226ca0784fa68cb0b6cc
sgallo <sofiagdelarosa@gmail.com>
2026-02-24 22:49:43 -0500

    Correccion about

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,4 +10,15 @@ class HomeController extends Controller
     {
         return view('home.index');
     }
+
+    public function about(): View
+    {
+        $viewData = [];
+        $viewData["title"] = "About us - Online Store";
+        $viewData["subtitle"] = "About us";
+        $viewData["description"] = "This is an about page ...";
+        $viewData["author"] = "Developed by: Your Name";
+        
+        return view('home.about')->with("viewData", $viewData);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'App\Http\Controllers\HomeController@index')->name("home.index");
-Route::get('/about', function () {
-    $data1 = "About us - Online Store";
-    $data2 = "About us";
-    $description = "This is an about page ...";
-    $author = "Developed by: Your Name";
-    return view('home.about')->with("title", $data1)
-      ->with("subtitle", $data2)
-      ->with("description", $description)
-      ->with("author", $author);
-})->name("home.about");
+Route::get('/about', 'App\Http\Controllers\HomeController@about')->name("home.about");
 
 
 // Orden importante: rutas específicas antes de las dinámicas
