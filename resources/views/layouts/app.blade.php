8f92388239504994dc1fe296649453118032bd4a
sgallo <sofiagdelarosa@gmail.com>
2026-03-03 19:29:57 -0500

    Actualizar banner para poder acceder a productos y carrito

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,6 +20,8 @@
         <div class="navbar-nav ms-auto">
           <a class="nav-link active" href="{{ route('home.index') }}">Home</a>
           <a class="nav-link active" href="{{ route('home.about') }}">About</a>
+          <a class="nav-link active" href="{{ route('product.index') }}">Products</a>
+          <a class="nav-link active" href="{{ route('cart.index') }}">Cart</a>
         </div>
       </div>
     </div>
