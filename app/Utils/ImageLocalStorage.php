5a8f16c006d71a5621d09ff46bb02fa64ef99605
sgallo <sofiagdelarosa@gmail.com>
2026-03-03 20:28:48 -0500

    Tutorial 3

---
+++ app/Utils/ImageLocalStorage.php
@@ -0,0 +1,20 @@
+<?php 
+
+namespace App\Utils;
+
+use App\Interfaces\ImageStorage;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+class ImageLocalStorage implements ImageStorage
+{
+    public function store(Request $request): void
+    {
+        if ($request->hasFile('profile_image')) {
+            Storage::disk('public')->put(
+                'test.png',
+                file_get_contents($request->file('profile_image')->getRealPath())
+            );
+        }
+    }
+}
